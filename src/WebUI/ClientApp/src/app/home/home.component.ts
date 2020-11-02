61826449d13f1f6fe793dfa28540865f3c41f359
parvez-syn <68910798+parvez-syn@users.noreply.github.com>
2020-11-02 12:43:16 +0530

    last min changes

--- src/WebUI/ClientApp/src/app/home/home.component.ts
+++ src/WebUI/ClientApp/src/app/home/home.component.ts
@@ -13,13 +13,13 @@ export class HomeComponent {
 
   ngOnInit() {
     this.bookData = [
-      { dealName: "Celanese",status: "Greenlight",reason: "NA",dealSize: "35000",dealType: "M&A",commitmentDate: "04/08/2020",launchDate: "06/10/2020" },
-      { dealName: "Amcor",status: "Greenlight",reason: "NA",dealSize: "35000",dealType: "M&A",commitmentDate: "04/08/2020",launchDate: "06/10/2020" },
-      { dealName: "Dupont",status: "Greenlight",reason: "NA",dealSize: "35000",dealType: "M&A",commitmentDate: "04/08/2020",launchDate: "06/10/2020" },
-      { dealName: "Graphic Packaging",status: "Greenlight",reason: "NA",dealSize: "35000",dealType: "M&A",commitmentDate: "04/08/2020",launchDate: "06/10/2020" },
-      { dealName: "LyondellBasell",status: "Greenlight",reason: "NA",dealSize: "35000",dealType: "M&A",commitmentDate: "04/08/2020",launchDate: "06/10/2020" }, 
-      { dealName: "Broke dealer",status: "Greenlight",reason: "NA",dealSize: "35000",dealType: "M&A",commitmentDate: "04/08/2020",launchDate: "06/10/2020" }, 
-      { dealName: "Graphic packaging",status: "Greenlight",reason: "NA",dealSize: "35000",dealType: "M&A",commitmentDate: "04/08/2020",launchDate: "06/10/2020" }
+      { dealName: "Celanese",status: "Pending",reason: "NA",dealSize: "35000",dealType: "M&A",commitmentDate: "04/08/2020",launchDate: "06/10/2020" },
+      { dealName: "Amcor",status: "Approved",reason: "NA",dealSize: "35000",dealType: "M&A",commitmentDate: "04/08/2020",launchDate: "06/10/2020" },
+      { dealName: "Dupont",status: "Pending",reason: "NA",dealSize: "35000",dealType: "M&A",commitmentDate: "04/08/2020",launchDate: "06/10/2020" },
+      { dealName: "Graphic Packaging",status: "Approved",reason: "NA",dealSize: "35000",dealType: "M&A",commitmentDate: "04/08/2020",launchDate: "06/10/2020" },
+      { dealName: "LyondellBasell",status: "Approved",reason: "NA",dealSize: "35000",dealType: "M&A",commitmentDate: "04/08/2020",launchDate: "06/10/2020" }, 
+      { dealName: "Broke dealer",status: "Pending",reason: "NA",dealSize: "35000",dealType: "M&A",commitmentDate: "04/08/2020",launchDate: "06/10/2020" }, 
+      { dealName: "Graphic packaging",status: "Approved",reason: "NA",dealSize: "35000",dealType: "M&A",commitmentDate: "04/08/2020",launchDate: "06/10/2020" }
     ]
   }
 
