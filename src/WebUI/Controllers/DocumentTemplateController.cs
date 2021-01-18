6ba0742bad4da1a3a9c59f607d3819b533ce9827
Pankaj-Bulbule <76563070+Pankaj-Bulbule@users.noreply.github.com>
2021-01-18 11:11:18 -0500

    DocumentTemplateController
    DocumentTemplateController

---
+++ src/WebUI/Controllers/DocumentTemplateController.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using DKP.InvestmentReview.Application.DocTemplates.Queries;
+
+namespace DKP.InvestmentReview.WebUI.Controllers
+{
+    public class DocumentTemplateController : ApiController{
+        
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DocTemplateDto>> GetDocument(int id){
+            //return await Mediator.Send(new GetDocTemplateQuery(){DocumentTemplateId = id});
+            return null;
+        }
+    }
+}
