ea0c6c2e2dd5d6ead5b0ec649fe27662706a0911
Prathamesh Kolhapure <prathamesh.kolhapure@synechron.com>
2021-01-19 08:20:43 -0800

    File upload initial changes

---
+++ src/WebUI/Controllers/ExcelWidgetController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+
+namespace DKP.InvestmentReview.WebUI.Controllers
+{
+    //[Route("api/[controller]")]
+    [ApiController]
+    public class ExcelWidgetController : ApiController
+    {
+        [HttpPost]
+        public IActionResult UploadFile(IFormFile formData)
+        {
+            var fileData = Request.Form.Files[0];
+            var pathToSave = Path.Combine(Directory.GetCurrentDirectory(), "Files");
+            if (fileData.Length > 0)
+            {
+                var fullPath = Path.Combine(pathToSave, fileData.FileName);
+                
+                using (var stream = new FileStream(fullPath, FileMode.Create))
+                {
+                    fileData.CopyTo(stream);
+                }
+                return Ok();
+            }
+            return BadRequest();
+        }
+    }
+}
